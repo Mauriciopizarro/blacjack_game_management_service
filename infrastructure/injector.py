23517f9d6bf3e3d3e0177d0c32c1dd9fbbfed1ad
Morena <pizarromauricio031096@gmail.com>
2022-12-07 20:47:35 -0300

    [MINOR] implementing rabbitmq

--- infrastructure/injector.py
+++ infrastructure/injector.py
@@ -1,10 +1,11 @@
+from infrastructure.event_managers.rabbit_publisher import RabbitPublisher
 from infrastructure.repositories.game_mongo_repository import GameMongoRepository as GameManagementMongoRepository
 from dependency_injector import containers, providers
 
 
 class Injector(containers.DeclarativeContainer):
-
     game_management_repo = providers.Singleton(GameManagementMongoRepository)
+    publisher = providers.Singleton(RabbitPublisher)
 
 
 injector = Injector()
